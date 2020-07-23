7d8e5e23c72d81e9f2f82624a1094b6d5aaeab27
Anas <anas@istikhor.com>
2020-07-23 09:08:50 +0800

    Completed lazy loading of images.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,5 @@
 import "../styles/styles.css";
+import "lazysizes";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
@@ -9,19 +10,21 @@ new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 new MobileMenu();
 let modal;
 
-document.querySelectorAll(".open-modal").forEach(el => {
-  el.addEventListener("click", e => {
-    e.preventDefault()
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
     if (typeof modal == "undefined") {
-      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
-        modal = new x.default()
-        setTimeout(() => modal.openTheModal(), 20)
-      }).catch(() => console.log("There was a problem."))
+      import(/* webpackChunkName: "modal" */ "./modules/Modal")
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log("There was a problem."));
     } else {
-      modal.openTheModal()
+      modal.openTheModal();
     }
-  })
-})
+  });
+});
 
 if (module.hot) {
   module.hot.accept();
